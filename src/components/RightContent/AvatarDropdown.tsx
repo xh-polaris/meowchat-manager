0b5650ec50c2e186ba6e3c35a1a5b615e298ea37
Yufeng Cheng <1351416566@qq.com>
2023-01-14 12:35:10 +0800

    fix: 修复退出登录问题

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,4 +1,4 @@
-import { outLogin } from '@/services/ant-design-pro/api';
+import { outLogin } from '@/services/auth';
 import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 import { history, useModel } from '@umijs/max';
 import { Avatar, Menu, Spin } from 'antd';
@@ -20,6 +20,7 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu = false }) => {
    */
   const loginOut = async () => {
     await outLogin();
+    localStorage.removeItem('accessToken');
     const { search, pathname } = window.location;
     const urlParams = new URL(window.location.href).searchParams;
     /** 此方法会跳转到 redirect 参数所在的位置 */
