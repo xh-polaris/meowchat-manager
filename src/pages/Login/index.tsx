d4ef08dffc71bf4aaae9b7c7b766d8b2de326bbd
oustr <biu_biu_@outlook.com>
2023-03-22 17:24:04 +1100

    feat: 初步实现微信登录接口和基本流程

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,118 +1,80 @@
 import Footer from '@/components/Footer';
-import { login } from '@/services/auth';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { LoginForm, ProFormCheckbox, ProFormText } from '@ant-design/pro-components';
+// import { login } from '@/services/auth';
+// import { LockOutlined, UserOutlined } from '@ant-design/icons';
+// import { LoginForm, ProFormCheckbox, ProFormText } from '@ant-design/pro-components';
 import { history, useModel } from '@umijs/max';
-import { message, Tabs } from 'antd';
+import { message } from 'antd';
 import { flushSync } from 'react-dom';
+import queryString from 'query-string';
 import styles from './index.less';
+import { weixinLogin } from '@/services/auth';
 
 const Login: React.FC = () => {
   const { initialState, setInitialState } = useModel('@@initialState');
 
-  const fetchUserInfo = async () => {
-    const userInfo = await initialState?.fetchUserInfo?.();
-    if (userInfo) {
-      flushSync(() => {
-        setInitialState((s) => ({
-          ...s,
-          currentUser: userInfo,
-        }));
-      });
-    }
-  };
+  const code: string = queryString.parse(location.search).code as string;
+  console.log(code);
 
-  const handleSubmit = async (values: API.LoginParams) => {
-    try {
-      // 登录
-      const msg = await login({ ...values });
-      if (msg.code === 0) {
-        localStorage.setItem('accessToken', `${msg.accessToken}`);
-        const defaultLoginSuccessMessage = '登录成功！';
-        message.success(defaultLoginSuccessMessage);
-        await fetchUserInfo();
-        const urlParams = new URL(window.location.href).searchParams;
-        history.push(urlParams.get('redirect') || '/');
-        return;
+  if (code !== undefined) {
+    const fetchUserInfo = async () => {
+      const userInfo = await initialState?.fetchUserInfo?.();
+      if (userInfo) {
+        flushSync(() => {
+          setInitialState((s) => ({
+            ...s,
+            currentUser: userInfo,
+          }));
+        });
       }
-      console.log(msg);
-    } catch (error) {
-      const defaultLoginFailureMessage = '登录失败，请重试！';
-      console.log(error);
-      message.error(defaultLoginFailureMessage);
-    }
-  };
+    };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.box}></div>
-      <div className={styles.content}>
-        <LoginForm
-          logo={<img alt="logo" src="https://static.xhpolaris.com/cat_world.jpg" />}
-          title="Meowchat 管理面板"
-          onFinish={async (values) => {
-            await handleSubmit(values as API.LoginParams);
-          }}
-        >
-          <Tabs
-            centered
-            items={[
-              {
-                key: 'account',
-                label: '账户密码登录',
-              },
-            ]}
-          />
-
-          <ProFormText
-            name="authId"
-            fieldProps={{
-              size: 'large',
-              prefix: <UserOutlined className={styles.prefixIcon} />,
-            }}
-            placeholder={'用户名'}
-            rules={[
-              {
-                required: true,
-                message: '请输入用户名!',
-              },
-            ]}
-          />
-          <ProFormText.Password
-            name="password"
-            fieldProps={{
-              size: 'large',
-              prefix: <LockOutlined className={styles.prefixIcon} />,
-            }}
-            placeholder={'密码'}
-            rules={[
-              {
-                required: true,
-                message: '请输入密码！',
-              },
-            ]}
-          />
-          <div
-            style={{
-              marginBottom: 24,
-            }}
-          >
-            <ProFormCheckbox noStyle name="autoLogin">
-              自动登录
-            </ProFormCheckbox>
+    const handleSubmit = async () => {
+      try {
+        // 登录
+        const msg = await weixinLogin(code);
+        console.log(msg);
+        if (msg.code === 0) {
+          localStorage.setItem('accessToken', `${msg.accessToken}`);
+          const defaultLoginSuccessMessage = '登录成功！';
+          message.success(defaultLoginSuccessMessage);
+          await fetchUserInfo();
+          const urlParams = new URL(window.location.href).searchParams;
+          history.push(urlParams.get('redirect') || '/');
+          return;
+        }
+        console.log(msg);
+      } catch (error) {
+        const defaultLoginFailureMessage = '登录失败，请重试！';
+        console.log(error);
+        message.error(defaultLoginFailureMessage);
+      }
+    };
+    handleSubmit();
+    return <div className={styles.center}>登陆中...</div>;
+  } else {
+    return (
+      <div className={styles.container}>
+        <div className={styles.box} />
+        <div className={styles.content}>
+          <div className={styles.main}>
+            <div className={styles.title}>
+              <img alt="logo" src="https://static.xhpolaris.com/cat_world.jpg" />
+              <span>Meowchat 管理面板</span>
+            </div>
             <a
-              style={{
-                float: 'right',
-              }}
+              href={
+                'https://open.weixin.qq.com/connect/qrconnect?appid=wx40ab73e6ebd6e636&redirect_uri=https://manager.meowchat.cn/login&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect'
+              }
+              className={styles.link}
             >
-              忘记密码
+              微信登陆
             </a>
           </div>
-        </LoginForm>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 };
 
 export default Login;
